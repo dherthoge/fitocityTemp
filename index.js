7f7d10e00171bbedd3b158344a4b81d16f8de725
Dylan Herthoge <dylanh.3006@gmail.com>
2022-09-29 23:36:09 -0400

    retry express

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const path = require('path');
-const generatePassword = require('password-generator');
-
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Use port from Heroku, fall back to default port
+const port = process.env.PORT || 3000;
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.listen(port, () => {
+  console.log(`App available at http://localhost:${port}`);
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Password generator listening on ${port}!`);
